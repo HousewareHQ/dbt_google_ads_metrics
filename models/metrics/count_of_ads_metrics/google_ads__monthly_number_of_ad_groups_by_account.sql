7865abcfa65c44ccac2451199bb51e01821d67aa
Sidhant Gupta <guptasidhant004@gmail.com>
2022-08-10 12:53:38 +0530

    Overview of changes: * Formatted SQL code in `models/google_ads_main.sql` * Removed redundant comments * Standardizing naming by changing `adgroup` to `ad_group` wherever referred

--- models/metrics/count_of_ads_metrics/google_ads__monthly_number_of_ad_groups_by_account.sql
+++ models/metrics/count_of_ads_metrics/google_ads__monthly_number_of_ad_groups_by_account.sql
@@ -4,8 +4,8 @@
 
 select *
 from {{ metrics.metric(
-  metric_name = 'google_ads__monthly_number_of_adgroups',
+  metric_name = 'google_ads__monthly_number_of_ad_groups_by_account',
   grain = 'month',
-  dimensions = [],
+  dimensions = ['account_name'],
   secondary_calculations = []
 ) }}
