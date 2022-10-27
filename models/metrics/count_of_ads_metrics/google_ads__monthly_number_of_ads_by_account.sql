0062aa5ac1d6ede35cbb978c232b739ebdd24b4d
Aryan Verma <54755677+AryanMadhavVerma@users.noreply.github.com>
2022-10-27 12:35:13 +0530

    Metrics coalesce update (#16)
    * update to dbt metrics 1.3.0
    
    Co-authored-by: Aryan Verma <aryanverma@Aryans-MacBook-Air.local>
    Co-authored-by: Aryan Verma <aryanverma@Aryans-Air.Dlink>

---
+++ models/metrics/count_of_ads_metrics/google_ads__monthly_number_of_ads_by_account.sql
@@ -0,0 +1,11 @@
+-- depends of {{ ref('google_ads_main') }}
+
+{{ config(materialized = 'table') }}
+
+select *
+from {{ metrics.calculate(
+  metric('google_ads__monthly_number_of_ads_by_account'),
+  grain = 'month',
+  dimensions = ['ad_group_name'],
+  secondary_calculations = []
+) }}
